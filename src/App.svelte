81e2c8184a1634ce78679bf140685521f8aa9b90
moumniheithem <moumniheithem@gmail.com>
2020-08-09 22:31:34 +0100

    fix app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,42 @@
 <script>
-	import Spinner from './Spinner.svelte'
-
-
+  import Spinner from "./Spinner.svelte";
 </script>
 
 <style>
+  code {
+    padding: 5px 5px;
+    margin-bottom: 35px;
+    display: block;
+    margin-top: 35px;
+    background-color: rgba(0, 0, 0, 0.04);
+    border-radius: 3px;
+    font-family: "Liberation Mono", Courier, monospace;
+  }
 </style>
 
-<Spinner name="circle"/>
-<br>sd<br>
-<Spinner name="pulse"/>
-<br>sd<br>
-<Spinner name="cube-grid" />
-<br>df<br>
-<Spinner name="wave" />
-<br>df<br>
-<Spinner name="folding-cube" />
-<br>
-sd
-<Spinner name="double-bounce" />
-<br>
-<Spinner name="rotating-plane" />s
+<h1>Svelte-Spinkit</h1>
+<h3>Spinkit Spinners</h3>
 
-<Spinner name="chasing-dots"></Spinner>
+<code>&lt;Spinner name="circle" /&gt;</code>
+<Spinner name="circle" />
+
+<code>&lt;Spinner name="pulse" /&gt;</code>
+<Spinner name="pulse" />
+
+<code>&lt;Spinner name="cube-grid" /&gt;</code>
+<Spinner name="cube-grid" />
+
+<code>&lt;Spinner name="wave" /&gt;</code>
+<Spinner name="wave" />
+
+<code>&lt;Spinner name="folding-cube" /&gt;</code>
+<Spinner name="folding-cube" />
+
+<code>&lt;Spinner name="double-bounce" /&gt;</code>
+<Spinner name="double-bounce" />
+
+<code>&lt;Spinner name="rotating-plane" /&gt;</code>
+<Spinner name="rotating-plane" />
+
+<code>&lt;Spinner name="chasing-dots" /&gt;</code>
+<Spinner name="chasing-dots" />
